09c26856bba55f9e54446f2d1add1dc016eefe5f
Вячеслав Нестеренко <dev@codencode.ru>
2024-06-07 16:53:53 +0300

    таблица с местами

--- places/models.py
+++ places/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Place(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField(max_length=256)
+    author = models.ForeignKey(User, on_delete=models.CASCADE) 
